727dd0eef53ea681856bd961b312320f479e4c21
hblinye <a1851yl@aiit.ac.jp>
2019-06-22 08:13:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# django-keycloak-demo
---
+++ demo/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from db.models import Inventory
+
+
+class InventorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Inventory
